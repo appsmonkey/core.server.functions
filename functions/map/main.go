ad01832551a95e33cd9706f336524e5a54f40648
smailg <smailgalijasevic@gmail.com>
2019-12-18 01:50:18 +0100

    fixed filtering no device

--- functions/map/main.go
+++ functions/map/main.go
@@ -79,28 +79,7 @@ func Handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse,
 		}
 	}
 
-	// var qry dal.ConditionBuilder
-	// hasFilter := false
-	// if request.Filter == "mine" {
-	// 	hasFilter = true
-	// 	qry = dal.Name("cognito_id").Equal(dal.Value(cognitoID))
-	// } else if request.Filter == "indoor" {
-	// 	hasFilter = true
-	// 	qry = dal.Name("indoor").Equal(dal.Value(true))
-	// } else if request.Filter == "outdoor" {
-	// 	hasFilter = true
-	// 	qry = dal.Name("indoor").Equal(dal.Value(false))
-	// }
-
-	// var dbRes *dal.ListResult
-	// var err error
-
-	// if  {
-	// 	fmt.Println("Query with filter")
-	// 	dbRes, err = dal.List("devices", qry, dal.Projection(dal.Name("token"), dal.Name("device_id"), dal.Name("meta"), dal.Name("map_meta"), dal.Name("active"), dal.Name("measurements"), dal.Name("cognito_id"), dal.Name("timestamp"), dal.Name("zone_id")))
-	// } else {
 	dbRes, err := dal.ListNoFilter("devices", dal.Projection(dal.Name("token"), dal.Name("device_id"), dal.Name("meta"), dal.Name("map_meta"), dal.Name("active"), dal.Name("measurements"), dal.Name("cognito_id"), dal.Name("timestamp"), dal.Name("zone_id")))
-	// }
 
 	dbData := make([]m.Device, 0)
 	err = dbRes.Unmarshal(&dbData)
@@ -122,9 +101,9 @@ func Handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse,
 				continue
 			} else if request.Filter == "mine" && !mine {
 				continue
-			} else if request.Filter == "outdoor" && d.Meta.Indoor == false {
+			} else if request.Filter == "indoor" && d.Meta.Indoor == false {
 				continue
-			} else if request.Filter == "indoor" && d.Meta.Indoor {
+			} else if request.Filter == "outdoor" && d.Meta.Indoor {
 				continue
 			}
 
