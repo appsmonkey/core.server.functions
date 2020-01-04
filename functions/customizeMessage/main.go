a91bf8c114d214d76105f602916b4d09d73e302a
smailg <smailgalijasevic@gmail.com>
2020-01-04 15:27:59 +0100

    changed greetings message

--- functions/customizeMessage/main.go
+++ functions/customizeMessage/main.go
@@ -13,14 +13,13 @@ func Handler(event events.CognitoEventUserPoolsCustomMessage) (events.CognitoEve
 	fmt.Println("CUSTOM MESSAGE REQ EVET CONTEXT: ", event.CallerContext.ClientID)
 
 	if event.TriggerSource == "CustomMessage_SignUp" {
-		event.Response.EmailMessage = "Welcome to CityOS, please click the following link to verify you email"
+		event.Response.EmailMessage = "Welcome to CityOS, please click the following link to verify your email, this is a custom message"
 		event.Response.EmailMessage = fmt.Sprintf(`Please click the link below to verify your email address. https://apigway.com/auth/validate?client_id=%s&user_name=%s&confirmation_code=%s`, event.CallerContext.ClientID, event.UserName, event.Request.CodeParameter)
 
 		return event.Response, nil
 	}
 
 	return event.Response, nil
-
 }
 
 func main() {
