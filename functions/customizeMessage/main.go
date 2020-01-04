281d5e5d9b23d9c1a8dff9defb4a736a6adac606
smailg <smailgalijasevic@gmail.com>
2020-01-04 20:06:34 +0100

    changed gopkg

--- functions/customizeMessage/main.go
+++ functions/customizeMessage/main.go
@@ -8,18 +8,18 @@ import (
 )
 
 // Handler will handle our request comming from the API gateway
-func Handler(event events.CognitoEvent) error {
+func Handler(event events.CognitoEventUserPoolsCustomMessage) (events.CognitoEventUserPoolsCustomMessageResponse, error) {
 	fmt.Println("CUSTOM MESSAGE REQ EVET: ", event)
-	// fmt.Println("CUSTOM MESSAGE REQ EVET CONTEXT: ", event.CallerContext.ClientID)
+	fmt.Println("CUSTOM MESSAGE REQ EVET CONTEXT: ", event.CallerContext.ClientID)
 
-	// if event.TriggerSource == "CustomMessage_SignUp" {
-	// 	event.Response.EmailMessage = "Welcome to CityOS, please click the following link to verify your email, this is a custom message"
-	// 	event.Response.EmailMessage = fmt.Sprintf(`Please click the link below to verify your email address. https://apigway.com/auth/validate?client_id=%s&user_name=%s&confirmation_code=%s`, event.CallerContext.ClientID, event.UserName, event.Request.CodeParameter)
+	if event.TriggerSource == "CustomMessage_SignUp" {
+		event.Response.EmailMessage = "Welcome to CityOS, please click the following link to verify your email, this is a custom message"
+		event.Response.EmailMessage = fmt.Sprintf(`Please click the link below to verify your email address. https://apigway.com/auth/validate?client_id=%s&user_name=%s&confirmation_code=%s`, event.CallerContext.ClientID, event.UserName, event.Request.CodeParameter)
 
-	// 	return event.Response, nil
-	// }
+		return event.Response, nil
+	}
 
-	return nil
+	return event.Response, nil
 }
 
 func main() {
