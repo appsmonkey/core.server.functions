41ab2bba1b2c5b62a2d199443c3abc5331700c95
smailg <smailgalijasevic@gmail.com>
2020-01-04 18:03:11 +0100

    added params to custom message lambda

--- functions/customizeMessage/main.go
+++ functions/customizeMessage/main.go
@@ -1,22 +1,27 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 // Handler will handle our request comming from the API gateway
-func Handler() error {
-	// fmt.Println("CUSTOM MESSAGE REQ EVET: ", event)
+func Handler(event events.CognitoEventUserPoolsCustomMessageRequest) (events.CognitoEventUserPoolsCustomMessageResponse, error) {
+	fmt.Println("CUSTOM MESSAGE REQ EVET: ", event)
 	// fmt.Println("CUSTOM MESSAGE REQ EVET CONTEXT: ", event.CallerContext.ClientID)
 
 	// if event.TriggerSource == "CustomMessage_SignUp" {
 	// 	event.Response.EmailMessage = "Welcome to CityOS, please click the following link to verify your email, this is a custom message"
 	// 	event.Response.EmailMessage = fmt.Sprintf(`Please click the link below to verify your email address. https://apigway.com/auth/validate?client_id=%s&user_name=%s&confirmation_code=%s`, event.CallerContext.ClientID, event.UserName, event.Request.CodeParameter)
 
-	// 	return  nil
+	// 	return event.Response, nil
 	// }
 
-	return nil
+	// return event.Response, nil
+
+	return events.CognitoEventUserPoolsCustomMessageResponse{}, nil
 }
 
 func main() {
