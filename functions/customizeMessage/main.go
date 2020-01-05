de01a2e1eae69d98c49e637f1306928aa8ff9372
smailg <smailgalijasevic@gmail.com>
2020-01-05 20:09:34 +0100

    added logger for customizeMessageRequest

--- functions/customizeMessage/main.go
+++ functions/customizeMessage/main.go
@@ -10,20 +10,17 @@ import (
 
 // Handler will handle our request comming from the API gateway
 func Handler(ctx context.Context, event events.CognitoEventUserPoolsCustomMessage) (events.CognitoEventUserPoolsCustomMessage, error) {
-	fmt.Println("CUSTOM MESSAGE REQ EVET: ", event)
+	fmt.Println("CUSTOM MESSAGE REQ EVENT: ", event)
 	fmt.Println("CUSTOM MESSAGE REQ CONTEXT: ", ctx)
-	fmt.Println("CUSTOM MESSAGE REQ EVET CONTEXT: ", event.CallerContext.ClientID)
+	fmt.Println("CUSTOM MESSAGE REQ EVENT CONTEXT: ", event.CallerContext.ClientID)
 
 	if event.TriggerSource == "CustomMessage_SignUp" {
-
 		fmt.Println("SIGNUP EVENT TRIGGERED")
 		event.Response.EmailMessage = "Welcome to CityOS, please click the following link to verify your email, this is a custom message"
 		event.Response.EmailMessage = fmt.Sprintf(`Please click the link below to verify your email address. https://apigway.com/auth/validate?client_id=%s&user_name=%s&confirmation_code=%s`, event.CallerContext.ClientID, event.UserName, event.Request.CodeParameter)
-
-		return event, nil
 	}
 
-	fmt.Println("RETURN STATEMENT")
+	fmt.Println("RETURN STATEMENT", event)
 	return event, nil
 }
 
