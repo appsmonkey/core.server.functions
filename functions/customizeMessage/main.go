4350e6f1099b6cc1dada3fbc31febf4d3dded4aa
smailg <smailgalijasevic@gmail.com>
2020-01-05 16:28:24 +0100

    added check

--- functions/customizeMessage/main.go
+++ functions/customizeMessage/main.go
@@ -13,6 +13,8 @@ func Handler(event events.CognitoEventUserPoolsCustomMessage) (events.CognitoEve
 	fmt.Println("CUSTOM MESSAGE REQ EVET CONTEXT: ", event.CallerContext.ClientID)
 
 	if event.TriggerSource == "CustomMessage_SignUp" {
+
+		fmt.Println("SIGNUP EVENT TRIGGERED")
 		event.Response.EmailMessage = "Welcome to CityOS, please click the following link to verify your email, this is a custom message"
 		event.Response.EmailMessage = fmt.Sprintf(`Please click the link below to verify your email address. https://apigway.com/auth/validate?client_id=%s&user_name=%s&confirmation_code=%s`, event.CallerContext.ClientID, event.UserName, event.Request.CodeParameter)
 
