752496583148660d07745ccac88fb8806236ff41
smailg <smailgalijasevic@gmail.com>
2020-01-05 19:28:00 +0100

    updated custom message lambda

--- functions/customizeMessage/main.go
+++ functions/customizeMessage/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 // Handler will handle our request comming from the API gateway
-func Handler(ctx context.Context, event events.CognitoEventUserPoolsCustomMessage) error {
+func Handler(ctx context.Context, event events.CognitoEventUserPoolsCustomMessage) (events.CognitoEventUserPoolsCustomMessage, error) {
 	fmt.Println("CUSTOM MESSAGE REQ EVET: ", event)
 	fmt.Println("CUSTOM MESSAGE REQ CONTEXT: ", ctx)
 	fmt.Println("CUSTOM MESSAGE REQ EVET CONTEXT: ", event.CallerContext.ClientID)
@@ -20,12 +20,11 @@ func Handler(ctx context.Context, event events.CognitoEventUserPoolsCustomMessag
 		event.Response.EmailMessage = "Welcome to CityOS, please click the following link to verify your email, this is a custom message"
 		event.Response.EmailMessage = fmt.Sprintf(`Please click the link below to verify your email address. https://apigway.com/auth/validate?client_id=%s&user_name=%s&confirmation_code=%s`, event.CallerContext.ClientID, event.UserName, event.Request.CodeParameter)
 
-		ctx.Done()
+		return event, nil
 	}
 
-	ctx.Done()
-
-	return nil
+	fmt.Println("RETURN STATEMENT")
+	return event, nil
 }
 
 func main() {
