a53addbda49dffb0f0a9bccf1816a96ccff8c342
smailg <smailgalijasevic@gmail.com>
2020-01-13 23:18:23 +0100

    added logger

--- models/schema/main.go
+++ models/schema/main.go
@@ -2,6 +2,7 @@ package schema
 
 import (
 	"encoding/json"
+	"fmt"
 	"math"
 )
 
@@ -82,6 +83,7 @@ type Data struct {
 // ConvertRawValue will return parsed raw value form device
 func (s *Data) ConvertRawValue(v float64) float64 {
 	if len(s.ParseCondition) > 0 {
+		fmt.Println("Parsing values ::: ", s.ParseCondition, v, s.Name)
 		switch s.ParseCondition {
 		case "round":
 			{
