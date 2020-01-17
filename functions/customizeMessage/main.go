622fefe869f77261c37d55810e3d98610df8d766
smailg <smailgalijasevic@gmail.com>
2020-01-17 23:07:31 +0100

    changed email subjects for verification mails

--- functions/customizeMessage/main.go
+++ functions/customizeMessage/main.go
@@ -14,14 +14,14 @@ func Handler(event events.CognitoEventUserPoolsCustomMessage) (events.CognitoEve
 	fmt.Println("USER SUB ::: ", userSub)
 
 	if event.TriggerSource == "CustomMessage_SignUp" {
-		event.Response.EmailMessage = "Welcome to CityOS, please click the following link to verify your email"
+		event.Response.EmailSubject = "Welcome to CityOS, please click the following link to verify your email"
 		event.Response.EmailMessage = fmt.Sprintf(
 			`Please click the link below to verify your email address.
 			https://links.cityos.io/auth/validate?client_id=%s&user_name=%s&confirmation_code=%s&type=verify&cog_id=%s`,
 			event.CallerContext.ClientID, event.UserName, event.Request.CodeParameter, userSub,
 		)
 	} else if event.TriggerSource == "CustomMessage_ForgotPassword" {
-		event.Response.EmailMessage = "Password reset requested"
+		event.Response.EmailSubject = "Password reset requested"
 		event.Response.EmailMessage = fmt.Sprintf(
 			`Password reset request, if this was you please go to the link below.
 
