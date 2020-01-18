2e1297e1255bbef24c3e3a7d99dbd8efb2ea0206
smailg <smailgalijasevic@gmail.com>
2020-01-18 14:47:47 +0100

    updated custom messages

--- functions/customizeMessage/main.go
+++ functions/customizeMessage/main.go
@@ -15,16 +15,30 @@ func Handler(event events.CognitoEventUserPoolsCustomMessage) (events.CognitoEve
 
 	if event.TriggerSource == "CustomMessage_SignUp" {
 		event.Response.EmailSubject = "Welcome to CityOS, please click the following link to verify your email"
-		event.Response.EmailMessage = "Please click the link below to verify your email address.\n\n" + fmt.Sprintf(
-			`https://links.cityos.io/auth/validate?client_id=%s&user_name=%s&confirmation_code=%s&type=verify&cog_id=%s`,
-			event.CallerContext.ClientID, event.UserName, event.Request.CodeParameter, userSub,
-		)
+		event.Response.EmailMessage = "Please click the link below to verify your email address.\n\n"
+		event.Response.EmailMessage += "\nhttps://links.cityos.io/auth/validate?client_id=" + event.CallerContext.ClientID
+		event.Response.EmailMessage += "&user_name=" + event.UserName
+		event.Response.EmailMessage += "&confirmation_code=" + event.Request.CodeParameter
+		event.Response.EmailMessage += "&type=verify&cog_id=" + userSub.(string)
+
+		// fmt.Sprintf(
+		// 	`https://links.cityos.io/auth/validate?client_id=%s&user_name=%s&confirmation_code=%s&type=verify&cog_id=%s`,
+		// 	event.CallerContext.ClientID, event.UserName, event.Request.CodeParameter, userSub,
+		// )
+
 	} else if event.TriggerSource == "CustomMessage_ForgotPassword" {
 		event.Response.EmailSubject = "Password reset requested"
-		event.Response.EmailMessage = "Password reset request, if this was you please go to the link below.\n\n" + fmt.Sprintf(
-			`https://links.cityos.io/auth/validate?client_id=%s&user_name=%s&confirmation_code=%s&type=pwreset&cog_id=%s`,
-			event.CallerContext.ClientID, event.UserName, event.Request.CodeParameter, userSub,
-		)
+		event.Response.EmailSubject = "Welcome to CityOS, please click the following link to verify your email"
+		event.Response.EmailMessage = "Password reset request, if this was you please go to the link below.\n\n"
+		event.Response.EmailMessage += "\nhttps://links.cityos.io/auth/validate?client_id=" + event.CallerContext.ClientID
+		event.Response.EmailMessage += "&user_name=" + event.UserName
+		event.Response.EmailMessage += "&confirmation_code=" + event.Request.CodeParameter
+		event.Response.EmailMessage += "&type=pwreset&cog_id=" + userSub.(string)
+
+		// event.Response.EmailMessage = "Password reset request, if this was you please go to the link below.\n\n" + fmt.Sprintf(
+		// 	`https://links.cityos.io/auth/validate?client_id=%s&user_name=%s&confirmation_code=%s&type=pwreset&cog_id=%s`,
+		// 	event.CallerContext.ClientID, event.UserName, event.Request.CodeParameter, userSub,
+		// )
 	}
 
 	return event, nil
