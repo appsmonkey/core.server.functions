2fedf3a70175704a432dc3f6cdd7a95213404b96
Albin Didic <albin.didic@gmail.com>
2020-02-03 23:35:21 +0100

    smoothing graph (compensating for spikes) test

--- functions/chartLiveAll/main.go
+++ functions/chartLiveAll/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 
 	"github.com/appsmonkey/core.server.functions/dal"
 	es "github.com/appsmonkey/core.server.functions/errorStatuses"
@@ -208,6 +209,183 @@ func qsortMulti(a []map[string]float64) []map[string]float64 {
 	qsortMulti(a[left+1:])
 
 	return a
+}
+
+func smooth(in []map[string]float64) []map[string]float64 {
+	result := make([]map[string]float64, 0)
+	lenIN := len(in)
+
+	for i := 0; i < lenIN; i++ {
+		result = append(result, in[i])
+
+		j := i + 1
+		if j == lenIN {
+			break
+		}
+
+		iDate := time.Unix(int64(in[i]["date"]), 0)
+		jDate := time.Unix(int64(in[j]["date"]), 0)
+		iValue := in[i]["value"]
+		jValue := in[j]["value"]
+
+		res := smoothPoints(iDate, jDate, iValue, jValue)
+		for _, dp := range res {
+			result = append(result, dp)
+		}
+	}
+
+	return result
+}
+
+func smoothPoints(it, jt time.Time, iv, jv float64) []map[string]float64 {
+	year, month, day, hour, min, _ := diff(it, jt)
+	minutes := float64(year*525600 + month*43800 + day*1440 + hour*60 + min)
+	res := make([]map[string]float64, 0)
+
+	// for large differences we can just add a simple curve
+	if year > 0 || month > 0 || day > 0 {
+		mod := float64(10)
+		v := iv
+		for {
+			// Get the time for the new data point (substract 10%)
+			m := time.Duration(minutes / mod)
+			t := it.Add(time.Minute * m * -1)
+
+			// Get the value for the new data point (substract 10%) of the difference between the two points
+			if iv > jv {
+				v -= (iv - jv) / mod
+			} else if iv < jv {
+				v += (iv - jv) / mod
+			}
+
+			// if we overshot, stop
+			if t.Before(jt) {
+				break
+			}
+
+			res = append(res, map[string]float64{
+				"date":  float64(t.Unix()),
+				"value": v,
+			})
+		}
+
+		return res
+	}
+
+	if hour > 0 {
+		mod := float64(5)
+		v := iv
+		for {
+			// Get the time for the new data point (substract 10%)
+			m := time.Duration(minutes / mod)
+			t := it.Add(time.Minute * m * -1)
+
+			// Get the value for the new data point (substract 10%) of the difference between the two points
+			if iv > jv {
+				v -= (iv - jv) / mod
+			} else if iv < jv {
+				v += (iv - jv) / mod
+			}
+
+			// if we overshot, stop
+			if t.Before(jt) {
+				break
+			}
+
+			res = append(res, map[string]float64{
+				"date":  float64(t.Unix()),
+				"value": v,
+			})
+		}
+
+		return res
+	}
+
+	// if we have three minutes missing, just do nothing
+	if min <= 3 {
+		return res
+	}
+
+	// we have a minutes chart so we need to figure out the amount of data point
+	// to put between the two existing points
+	// we base it on the minimum value jump in our dataset
+	if min > 3 {
+		mod := float64(min)
+		v := iv
+		for {
+			// Get the time for the new data point (substract 10%)
+			m := time.Duration(minutes / mod)
+			t := it.Add(time.Minute * m * -1)
+
+			// Get the value for the new data point (substract 10%) of the difference between the two points
+			if iv > jv {
+				v -= (iv - jv) / mod
+			} else if iv < jv {
+				v += (iv - jv) / mod
+			}
+
+			// if we overshot, stop
+			if t.Before(jt) {
+				break
+			}
+
+			res = append(res, map[string]float64{
+				"date":  float64(t.Unix()),
+				"value": v,
+			})
+		}
+
+		return res
+	}
+
+	return res
+}
+
+func diff(a, b time.Time) (year, month, day, hour, min, sec int) {
+	if a.Location() != b.Location() {
+		b = b.In(a.Location())
+	}
+	if a.After(b) {
+		a, b = b, a
+	}
+	y1, M1, d1 := a.Date()
+	y2, M2, d2 := b.Date()
+
+	h1, m1, s1 := a.Clock()
+	h2, m2, s2 := b.Clock()
+
+	year = int(y2 - y1)
+	month = int(M2 - M1)
+	day = int(d2 - d1)
+	hour = int(h2 - h1)
+	min = int(m2 - m1)
+	sec = int(s2 - s1)
+
+	// Normalize negative values
+	if sec < 0 {
+		sec += 60
+		min--
+	}
+	if min < 0 {
+		min += 60
+		hour--
+	}
+	if hour < 0 {
+		hour += 24
+		day--
+	}
+	if day < 0 {
+		// days in month:
+		t := time.Date(y1, M1, 32, 0, 0, 0, 0, time.UTC)
+		day += 32 - t.Day()
+		month--
+	}
+	if month < 0 {
+		month += 12
+		year--
+	}
+
+	return
 }
 
 func main() {
