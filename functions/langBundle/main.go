2cdf2d147312d7dd1968a82e9473e45a9fb2471d
smailg <smailgalijasevic@gmail.com>
2020-02-05 00:02:30 +0100

    update link for getlang

--- functions/langBundle/main.go
+++ functions/langBundle/main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"log"
 	"os"
 
 	"github.com/appsmonkey/core.server.functions/integration/cognito"
@@ -12,7 +9,6 @@ import (
 	vm "github.com/appsmonkey/core.server.functions/viewmodels"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/joho/godotenv"
 )
 
 var (
@@ -28,37 +24,8 @@ func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.API
 	return events.APIGatewayProxyResponse{Body: response.Marshal(), StatusCode: 200, Headers: response.Headers()}, nil
 }
 
-func init() {
-	if os.Getenv("ENV") == "local" {
-		err := godotenv.Load(".env")
-		if err != nil {
-			log.Fatalf("error loading .env: %v\n", err)
-		}
-	}
-
-	cog = cognito.NewCognito()
-}
-
-func local() {
-	data, _ := json.Marshal(vm.ForgotPasswordStartRequest{
-		Email: os.Getenv("USER_EMAIL"),
-	})
-
-	resp, err := Handler(context.Background(), events.APIGatewayProxyRequest{
-		Body: string(data),
-	})
-
-	if err != nil {
-		fmt.Printf("unhandled error! \nError: %v\n", err)
-	} else {
-		j, _ := json.MarshalIndent(resp, "", "  ")
-		fmt.Println(string(j))
-	}
-}
-
 func main() {
 	if os.Getenv("ENV") == "local" {
-		local()
 		return
 	}
 
