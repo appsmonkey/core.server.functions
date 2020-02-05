ba799943e82461ab143cae93037b809cdc328506
smailg <smailgalijasevic@gmail.com>
2020-02-05 23:01:36 +0100

    Updated logger

--- functions/map/main.go
+++ functions/map/main.go
@@ -90,6 +90,7 @@ func Handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse,
 				t.Data = append(t.Data, zz.Data)
 				zoneMap[zz.ZoneID] = t
 			} else {
+				fmt.Println("ZONE_NAME ::: ", zz.ZoneName)
 				zoneMap[zz.ZoneID] = zoneResult{ZoneID: zz.ZoneID, Data: []m.ZoneMeta{zz.Data}, ZoneName: zz.ZoneName}
 			}
 		}
