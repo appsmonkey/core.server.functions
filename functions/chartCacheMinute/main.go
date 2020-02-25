e7cfd06bde1012a2e4a0b0ea2d5765b099bbe887
Smail Galijasevic <smail@ministryofprogramming.com>
2020-02-25 14:03:10 +0100

    fix: live charts for other cities

--- functions/chartCacheMinute/main.go
+++ functions/chartCacheMinute/main.go
@@ -33,62 +33,62 @@ func Handler(ctx context.Context, req interface{}) error {
 		fmt.Println("Unmarshaling error ::: ", err)
 	}
 
-	// n := len(cities)
-	// sem := make(chan empty, n) // Using semaphore for efficiency
+	n := len(cities)
+	sem := make(chan empty, n) // Using semaphore for efficiency
 
-	// for _, key := range cities {
-	// 	go func(key m.City) {
+	for _, key := range cities {
+		go func(key m.City) {
 
-	// 		dd := defaultDevice.Get(key.CityID)
-	// 		then := time.Now()
-	// 		data := make(map[string]interface{}, 0)
+			dd := defaultDevice.Get(key.CityID)
+			then := time.Now()
+			data := make(map[string]interface{}, 0)
 
-	// 		data["token"] = dd.City
-	// 		data["timestamp"] = then.Unix()
-	// 		data["timestamp_sort"] = formulateTimestamp(then.Unix()).Unix()
-	// 		data["ttl"] = then.Add(time.Hour * 6).Unix()
-	// 		data["indoor"] = false
+			data["token"] = dd.City
+			data["timestamp"] = then.Unix()
+			data["timestamp_sort"] = formulateTimestamp(then.Unix()).Unix()
+			data["ttl"] = then.Add(time.Hour * 6).Unix()
+			data["indoor"] = false
 
-	// 		for k, v := range dd.Latest {
-	// 			data[k] = v
-	// 		}
+			for k, v := range dd.Latest {
+				data[k] = v
+			}
 
-	// 		err = dal.Insert("chart_all_minute", data)
-	// 		if err != nil {
-	// 			fmt.Println("Couldn't insert data into table")
-	// 		}
-	// 		sem <- empty{}
-	// 	}(key)
-	// } - use this wehn list table stops using scan calls
+			err = dal.Insert("chart_all_minute", data)
+			if err != nil {
+				fmt.Println("Couldn't insert data into table")
+			}
+			sem <- empty{}
+		}(key)
+	}
+
+	// -------------------------------------------
 
 	// dd := defaultDevice.Get("Sarajevo")
 	// then := time.Now()
 	// data := make(map[string]interface{}, 0)
 
-	dd := defaultDevice.Get("Sarajevo")
-	then := time.Now()
-	data := make(map[string]interface{}, 0)
+	// data["token"] = dd.City
+	// data["timestamp"] = then.Unix()
+	// data["timestamp_sort"] = formulateTimestamp(then.Unix()).Unix()
+	// data["ttl"] = then.Add(time.Hour * 6).Unix()
+	// data["indoor"] = false
 
-	data["token"] = dd.City
-	data["timestamp"] = then.Unix()
-	data["timestamp_sort"] = formulateTimestamp(then.Unix()).Unix()
-	data["ttl"] = then.Add(time.Hour * 6).Unix()
-	data["indoor"] = false
+	// for k, v := range dd.Latest {
+	// 	data[k] = v
+	// }
 
-	for k, v := range dd.Latest {
-		data[k] = v
-	}
+	// err = dal.Insert("chart_all_minute", data)
+	// if err != nil {
+	// 	fmt.Println("Couldn't insert data into table")
+	// 	return err
+	// }
 
-	err = dal.Insert("chart_all_minute", data)
-	if err != nil {
-		fmt.Println("Couldn't insert data into table")
-		return err
-	}
+	// -------------------------------------------
 
 	// wait for goroutines to finish
-	// for i := 0; i < n; i++ {
-	// 	<-sem
-	// }
+	for i := 0; i < n; i++ {
+		<-sem
+	}
 
 	return nil
 }
